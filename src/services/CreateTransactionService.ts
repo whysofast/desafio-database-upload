f8061b860ef75e39e4d955446ee092a58b4fbae9
Matheus Carvalho <matheusvca@gmail.com>
2020-11-13 02:40:08 -0300

    3/7 tests. Create Category model and migration. Create Transaction model, migration, createService and POSTroute

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,42 @@
+import { getCustomRepository, getRepository } from 'typeorm';
 // import AppError from '../errors/AppError';
 
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({ title, value, type, category }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+    const categoryRepository = getRepository(Category);
+
+    let categoryFound = await categoryRepository.findOne({ where: { title: category } });
+
+    if (!categoryFound) {
+      categoryFound = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(categoryFound);
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category: categoryFound,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
