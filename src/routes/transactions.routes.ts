80eef9a0922ce911ccfa51707fd5e4658c65440c
WhySoFast <matheusvca@msn.com>
2020-11-15 20:02:08 -0300

    Initial implmenet import Transaction.

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
+import multer from 'multer';
 import CreateTransactionService from '../services/CreateTransactionService';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import DeleteTransactionService from '../services/DeleteTransactionService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
+import uploadConfig from '../config/upload';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+const upload = multer(uploadConfig);
 
 const transactionsRouter = Router();
 
@@ -44,8 +45,12 @@ transactionsRouter.delete('/:id', async (request, response) => {
   return response.status(204).json(transaction);
 });
 
-// transactionsRouter.post('/import', async (request, response) => {
-//   // TODO
-// });
+transactionsRouter.post('/import', upload.single('file'), async (request, response) => {
+  const importTransaction = new ImportTransactionsService();
+
+  const transactions = await importTransaction.execute(request.file.path);
+
+  return response.json(transactions);
+});
 
 export default transactionsRouter;
